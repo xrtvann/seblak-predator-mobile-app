595d165fd85cbfc3ed1a8c1447b161587819c7a0
Muhammad Fitrah Abdul Rahman Saleh <mfitrah15ofcc@gmail.com>
2025-09-25 15:04:58 +0700

    backupan

--- app/src/main/java/com/irvan/seblakpredator/ui/theme/LoginActivity.java
+++ app/src/main/java/com/irvan/seblakpredator/ui/theme/LoginActivity.java
@@ -1,9 +1,14 @@
 package com.irvan.seblakpredator.ui.theme;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MotionEvent;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -22,5 +27,37 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        EditText passwordText = findViewById(R.id.passwordColumn);
+        final boolean[] isPasswordVisible = {false};
+        Typeface typeface = ResourcesCompat.getFont(this, R.font.arial);
+        passwordText.setTypeface(typeface);
+
+        passwordText.setOnTouchListener((v, event) ->{
+            final int drawable_right = 2;
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                if (event.getRawX() >= (passwordText.getRight()
+                        - passwordText.getCompoundDrawables()[drawable_right].getBounds().width())) {
+
+                    if (isPasswordVisible[0]) {
+                        // Sembunyikan password
+                        passwordText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                        passwordText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_eyehide, 0);
+                        isPasswordVisible[0] = false;
+                    } else {
+                        // Tampilkan password
+                        passwordText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                        passwordText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_eyeshow, 0);
+                        isPasswordVisible[0] = true;
+                    }
+
+                    // Biar cursor tetap di akhir teks
+                    passwordText.setTypeface(ResourcesCompat.getFont(this, R.font.arial));
+                    passwordText.setSelection(passwordText.getText().length());
+                    return true;
+                }
+            }
+            return false;
+        } );
     }
 }
--- app/src/main/java/com/irvan/seblakpredator/ui/theme/RegisterActivity.java
+++ app/src/main/java/com/irvan/seblakpredator/ui/theme/RegisterActivity.java
@@ -1,8 +1,6 @@
-package com.irvan.seblakpredator.splash;
+package com.irvan.seblakpredator.ui.theme;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,33 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.irvan.seblakpredator.MainActivity;
 import com.irvan.seblakpredator.R;
-import com.irvan.seblakpredator.ui.theme.LoginActivity;
 
-public class SplashActivity extends AppCompatActivity {
-
-    Handler handler = new Handler();
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }, 2000);
     }
 }
