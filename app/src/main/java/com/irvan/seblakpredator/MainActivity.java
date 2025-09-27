0ed7b9c4ac684adcb9604f45d604882423ebab44
xrtvann <irvangoblas@gmail.com>
2025-09-27 11:39:46 +0700

    update: navbar using style material 3

--- app/src/main/java/com/irvan/seblakpredator/MainActivity.java
+++ app/src/main/java/com/irvan/seblakpredator/MainActivity.java
@@ -1,10 +1,9 @@
 package com.irvan.seblakpredator;
 
 import android.os.Bundle;
-
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -12,8 +11,10 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.irvan.seblakpredator.fragment.DashboardFragment;
 import com.irvan.seblakpredator.fragment.PengaturanFragment;
+import com.irvan.seblakpredator.fragment.RiwayatFragment;
 import com.irvan.seblakpredator.fragment.TransaksiFragment;
 
 
@@ -23,15 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         bottomNavigation = findViewById(R.id.bottom_navigation);
 
@@ -42,14 +36,14 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigation.setOnItemSelectedListener(menuItem -> {
             Fragment selectedFragment = null;
-
             int id = menuItem.getItemId();
-
 
             if (id == R.id.nav_home) {
                 selectedFragment = new DashboardFragment();
             } else if (id == R.id.nav_transaction) {
                 selectedFragment = new TransaksiFragment();
+            } else if (id == R.id.nav_history) {
+                selectedFragment = new RiwayatFragment();
             } else if (id == R.id.nav_setting) {
                 selectedFragment = new PengaturanFragment();
             }
@@ -58,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
                 getSupportFragmentManager().beginTransaction()
                         .replace(R.id.frame_container, selectedFragment)
                         .commit();
-
             }
             return true;
         });
