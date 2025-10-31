a675511ed5b31115cfd8c3e9eeaff43338688c1f
Muhammad Fitrah Abdul Rahman Saleh <mfitrah15ofcc@gmail.com>
2025-10-31 13:56:10 +0700

    update androidmanifest , dashboard (bagian cardView menggunakan layout baru , menerapkan effect di setiap tombol untuk mengalih tampilan)

--- app/src/main/java/com/irvan/seblakpredator/MainActivity.java
+++ app/src/main/java/com/irvan/seblakpredator/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.irvan.seblakpredator.fragment.DashboardFragment;
@@ -11,9 +12,21 @@ import com.irvan.seblakpredator.fragment.PengaturanFragment;
 import com.irvan.seblakpredator.fragment.RiwayatFragment;
 import com.irvan.seblakpredator.fragment.TransaksiFragment;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigation;
+    int lastPosition = 0; // posisi terakhir tombol
+
+    // urutan tombol (kiri → kanan)
+    Map<Integer, Integer> navOrder = new HashMap<Integer, Integer>() {{
+        put(R.id.nav_home, 0);
+        put(R.id.nav_transaction, 1);
+        put(R.id.nav_history, 2);
+        put(R.id.nav_setting, 3);
+    }};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +57,25 @@ public class MainActivity extends AppCompatActivity {
             }
 
             if (selectedFragment != null) {
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.frame_container, selectedFragment)
-                        .commit();
+                int newPosition = navOrder.get(id);
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                if (newPosition > lastPosition) {
+                    // geser ke kanan (next)
+                    transaction.setCustomAnimations(
+                            R.anim.slide_in_right, // masuk dari kanan
+                            R.anim.slide_out_left  // keluar ke kiri
+                    );
+                } else if (newPosition < lastPosition) {
+                    // geser ke kiri (back)
+                    transaction.setCustomAnimations(
+                            R.anim.slide_in_left,  // masuk dari kiri
+                            R.anim.slide_out_right // keluar ke kanan
+                    );
+                }
+
+                transaction.replace(R.id.frame_container, selectedFragment).commit();
+                lastPosition = newPosition;
             }
 
             return true;
